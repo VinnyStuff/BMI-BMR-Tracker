acba8b6293b130bafbfeb183d086a12ba877ee78
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-10-06 16:01:50 -0300

    pegando valores dos inputs

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("buttonCal").onclick = calculate;
+
+function calculate(){
+    const age = document.getElementById("age").value;
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    console.log(age);
+    console.log(height);
+    console.log(weight);
+    console.log(gender);
+}
