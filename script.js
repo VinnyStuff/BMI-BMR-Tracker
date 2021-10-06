97f461ab96ce4d429a684ea2ad537801b3b66651
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-10-06 17:53:05 -0300

    iniciando taxa metabolica basal

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 window.onload = function() {
-    document.getElementById("buttonCal").onclick = calculate;
+    document.getElementById("buttonCal").onclick = getTheValuesAndCalculate;
 };
 
-function calculate(){
+function getTheValuesAndCalculate(){
     const age = Number(document.getElementById("age").value);
     const heightFirstInput = Number(document.getElementsByClassName("height")[0].value);
     const heightSecondInput = Number(document.getElementsByClassName("height")[1].value);
     const weight = Number(document.getElementById("weight").value);
     const gender = (document.querySelector('input[name="gender"]:checked') != null) ? document.querySelector('input[name="gender"]:checked').value : null;
 
-    //BMI or IMC
-    const bmiHeight = heightFirstInput + (heightSecondInput * 0.01);
-    const bmi = weight / (bmiHeight * bmiHeight);
+    bmi(weight , heightFirstInput, heightSecondInput);
 
-    console.log(weight);
-    console.log(bmiHeight);
+    bmr(age, heightFirstInput, heightSecondInput, weight, gender);
+}
+
+function bmi(weight, heightFirstInput, heightSecondInput){
+    const height = heightFirstInput + (heightSecondInput * 0.01);
+
+    const bmi = weight / (height * height);
     console.log(bmi);
+}
+
+function bmr(age, heightFirstInput, heightSecondInput, weight, gender){
+    height = (heightFirstInput * 100) + heightSecondInput;
+    if (gender == "male"){
+        console.log(height);
+        const bmr = 66.5 + (13.75 * weight) + (5.0 * height) - (6.8 * age);
+        //VER ESSA FORMULA NOVAMENTE
+        console.log(bmr);
+    }
+    else if (gender == "female"){
+
+    }
 }
