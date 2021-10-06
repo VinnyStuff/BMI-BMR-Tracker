7f93141147f1aee08cc5c9efef4fd24162ce017e
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-10-06 16:23:00 -0300

    conseguindo todos os valores - com os generos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculate(){
     const age = document.getElementById("age").value;
     const height = document.getElementById("height").value;
     const weight = document.getElementById("weight").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const gender = (document.querySelector('input[name="gender"]:checked') != null) ? document.querySelector('input[name="gender"]:checked').value : null;
 
     console.log(age);
     console.log(height);
