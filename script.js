cbb12934249cab3cfd9e5b015e03e945f005c0e8
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-10-08 00:59:36 -0300

    bmi pronto

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function getTheValuesAndCalculate(){
     const heightSecondInput = Number(document.getElementsByClassName("height")[1].value);
     const weight = Number(document.getElementById("weight").value);
     const gender = (document.querySelector('input[name="gender"]:checked') != null) ? document.querySelector('input[name="gender"]:checked').value : null;
+    const data = [age, heightFirstInput, heightSecondInput, weight, gender];
+
+    for (let i = 0; i < data.length; i++){
+        
+    }
 
     bmi(weight , heightFirstInput, heightSecondInput);
 
@@ -18,18 +23,38 @@ function bmi(weight, heightFirstInput, heightSecondInput){
     const height = heightFirstInput + (heightSecondInput * 0.01);
 
     const bmi = weight / (height * height);
-    console.log(bmi);
+
+    const classification = function (){
+        if (bmi < 18.5){
+            return "Underweight";
+        }
+        else if (bmi > 18.5 && bmi < 24.9){
+            return "Normal weight";
+        }
+        else if (bmi > 25.0 && bmi < 29.9){
+            return "Pre-obesity";
+        }
+        else if (bmi > 30.0 && bmi < 34.9){
+            return "Obesity class I";
+        }
+        else if (bmi > 35.0 && bmi < 39.9){
+            return "Obesity class II";
+        }
+        else if (bmi > 40.0){
+            return "Obesity class III";
+        }
+    }
+
+    //console.log(bmi);
+    //console.log(classification());
 }
 
 function bmr(age, heightFirstInput, heightSecondInput, weight, gender){
     height = (heightFirstInput * 100) + heightSecondInput;
     if (gender == "male"){
-        console.log(height);
-        const bmr = 66.5 + (13.75 * weight) + (5.0 * height) - (6.8 * age);
-        //VER ESSA FORMULA NOVAMENTE
-        console.log(bmr);
+
     }
-    else if (gender == "female"){
+    else if (gender == "female"){  
 
     }
 }
